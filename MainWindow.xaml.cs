d51c018864469ed4503fe545a0a6e049077561bf
Vadim Barkhatov <VBarkhatov@cheo.on.ca>
2019-09-26 13:17:45 -0400

    RAS testing

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,6 +20,11 @@ using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
 
+using CrystalDecisions.Enterprise;
+using CrystalDecisions.ReportAppServer.ClientDoc;
+using CrystalDecisions.ReportAppServer.Controllers;
+using CrystalDecisions.ReportAppServer.ReportDefModel;
+
 namespace CHEORptAnalyzer
 {
     /// <summary>
@@ -29,9 +35,9 @@ namespace CHEORptAnalyzer
     {
         XElement xroot;
 
-        public bool SearchFields { get; set; }
-        public bool SearchRF { get; set; }
-        public bool SearchCommand { get; set; }
+        public bool SearchFields { get; set; } = true;
+        public bool SearchRF { get; set; } = true;
+        public bool SearchCommand { get; set; } = true;
 
 
         public MainWindow()
@@ -52,31 +58,19 @@ namespace CHEORptAnalyzer
             }
 
             DataContext = this;
+
+            tbPreview.Document.PageWidth = 1000;
         }
 
 
 
         private void Button_Click(object sender, RoutedEventArgs events)
         {
-
-            //.Elements("Database").Elements("Tables").Elements("Table")
-            //.Elements("Database").Elements("Tables").Elements("Table").Elements("Fields").Elements("Field")
-            //ClassName = "CrystalReports.CommandTable"
-
             var searchString = tbSearch.Text;
-
-            //Func<XElement, XAttribute> attributes
-
-            //XAttribute x2 = new XAttribute();
-
-            //x2.
-            //IEnumerable<XAttribute> attrFilter(XElement node) => node.Descendants("Field").Attributes("FormulaName").Where(x => x.Value.ToUpper().Contains(searchString.ToUpper()));
 
             Func<string, bool> textFilter = s => s.ToUpper().Contains(searchString.ToUpper());
 
             Func<XElement, IEnumerable<XElement>> attrFilter;
-
-            //attrFilter = node => node.Descendants("Field").Attributes("FormulaName").Where(x => textFilter(x.Value));
 
             Func<XElement, IEnumerable<XElement>> fieldFilter = node => node.Descendants("Field").Where(x => x.Attribute("FormulaName") != null && textFilter(x.Attribute("FormulaName").Value));
             Func<XElement, IEnumerable<XElement>> cmdFilter = node => node.Descendants("Command").Where(x => textFilter(x.Value));
@@ -84,18 +78,18 @@ namespace CHEORptAnalyzer
 
             attrFilter = node => fieldFilter(node).Union(cmdFilter(node)).Union(rcdFilter(node));
             
-
-
             IEnumerable<XElement> foundReports = xroot.Elements("Report").Where(x => attrFilter(x).Count() > 0);
-
 
             xroot.Descendants();
             lbReports.Items.Clear();
 
+            
             foreach (XElement e in foundReports)
             {
-                lbReports.Items.Add(new ListTuple<XElement>() { Text = e.Attribute("FileName").Value, Obj = e});
-                Trace.WriteLine((e as IXmlLineInfo).HasLineInfo());
+                var results = attrFilter(e).Select(x => x.Value).ToList();
+                results.RemoveAll(x => x == "");
+                
+                lbReports.Items.Add(new ListTuple<XElement>() { Text = e.Attribute("FileName").Value, Obj = e, SearchResults = results });
             }
         }
 
@@ -112,8 +106,137 @@ namespace CHEORptAnalyzer
         {
             var selReport = (lbReports.SelectedItem as ListTuple<XElement>)?.Obj;
 
-            tbPreview.Text = selReport?.ToString();
+            
+
+            tbPreview.Document.Blocks.Clear();
+            tbPreview.AppendText(selReport?.ToString());
+
+
+            var searchResults = (lbReports.SelectedItem as ListTuple<XElement>)?.SearchResults;
+            
+            if(searchResults != null)
+            { 
+                foreach (string f in searchResults)
+                {
+                    //Trace.WriteLine(f);
+                    //Highlighter(f);
+                }
+            }
+
+            Highlighter("INNER JOIN Clarity..CLARITY_SER d on a.BILLING_PROVIDER_ID = d.PROV_ID");
         }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            RetrieveReport();
+        }
+
+        EnterpriseSession boEnterpriseSession;
+        InfoObject boInfoObject;
+        ReportClientDocument boReportClientDocument;
+
+        private void RetrieveReport()
+        {
+            SessionMgr boSessionMgr;
+            InfoStore boInfoStore;
+            EnterpriseService boEnterpriseService;
+            InfoObjects boInfoObjects;
+            string boReportName;
+            string boQuery;
+            CrystalDecisions.ReportAppServer.ClientDoc.ReportAppFactory boReportAppFactory;
+
+            //Log on to the Enterprise CMS
+            boSessionMgr = new CrystalDecisions.Enterprise.SessionMgr();
+            //boEnterpriseSession = boSessionMgr.Logon(Request.QueryString["username"], Request.QueryString["password"], Request.QueryString["cms"], Request.QueryString["authtype"]);
+            boEnterpriseSession = boSessionMgr.Logon("vbarkhatov", "wat", "boeappprd", "Enterprise");
+            //Session.Add("boEnterpriseSession", boEnterpriseSession);
+            boEnterpriseService = boEnterpriseSession.GetService("", "InfoStore");
+            boInfoStore = new CrystalDecisions.Enterprise.InfoStore(boEnterpriseService);
+
+            boReportName = "Daily Non Violent Crisis Intervention Skill (NVC3).rpt";
+
+            //Retrieve the report object from the InfoStore, only need the SI_ID for RAS
+            boQuery = "Select SI_ID From CI_INFOOBJECTS Where SI_NAME = '" + boReportName +
+                "' AND SI_Instance=0";
+            boInfoObjects = boInfoStore.Query(boQuery);
+            boInfoObject = boInfoObjects[1];
+
+            boEnterpriseService = null;
+
+            //Retrieve the RASReportFactory
+            boEnterpriseService = boEnterpriseSession.GetService("RASReportFactory");
+            boReportAppFactory = (CrystalDecisions.ReportAppServer.ClientDoc.ReportAppFactory)boEnterpriseService.Interface;
+            //Open the report from Enterprise
+            boReportClientDocument = boReportAppFactory.OpenDocument(boInfoObject.ID, 0);
+
+            /**
+             * This exports the report to a byte() that we will stream out using the default options for the enum
+             * The available enums are:
+             * CrReportExportFormatEnum.crReportExportFormatCharacterSeparatedValues
+             * CrReportExportFormatEnum.crReportExportFormatCrystalReports
+             * CrReportExportFormatEnum.crReportExportFormatEditableRTF
+             * CrReportExportFormatEnum.crReportExportFormatHTML
+             * CrReportExportFormatEnum.crReportExportFormatMSExcel
+             * CrReportExportFormatEnum.crReportExportFormatMSWord
+             * CrReportExportFormatEnum.crReportExportFormatPDF
+             * CrReportExportFormatEnum.crReportExportFormatRecordToMSExcel
+             * CrReportExportFormatEnum.crReportExportFormatRTF
+             * CrReportExportFormatEnum.crReportExportFormatTabSeparatedText
+             * CrReportExportFormatEnum.crReportExportFormatText
+             * CrReportExportFormatEnum.crReportExportFormatXML
+            */
+            CrystalDecisions.ReportAppServer.CommonObjectModel.ByteArray boByteArray = boReportClientDocument.PrintOutputController.Export(CrReportExportFormatEnum.crReportExportFormatCrystalReports, 1);
+            //Save the ByteArray to disk, overwriting any existing file with the same name.
+            boByteArray.Save(@"c:\test\myExport.rpt", true);
+
+            //Response.Write(@"File successfully saved to C:\Windows\Temp\myExport.pdf");
+
+
+            boReportClientDocument.Close();
+            boEnterpriseSession.Logoff();
+        }
+
+
+        private void Highlighter(string searchText)
+        {
+            tbPreview.SelectAll();
+            tbPreview.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(Colors.Black));
+            tbPreview.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Normal);
+
+            Regex reg = new Regex(searchText, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            TextPointer position = tbPreview.Document.ContentStart;
+            List<TextRange> ranges = new List<TextRange>();
+
+            while (position != null)
+            {
+                if (position.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.Text)
+                {
+                    string text = position.GetTextInRun(LogicalDirection.Forward);
+                    var matchs = reg.Matches(text);
+
+                    foreach (Match match in matchs)
+                    {
+
+                        TextPointer start = position.GetPositionAtOffset(match.Index);
+                        TextPointer end = start.GetPositionAtOffset(searchText.Length);
+
+                        TextRange textrange = new TextRange(start, end);
+                        ranges.Add(textrange);
+                    }
+                }
+                position = position.GetNextContextPosition(LogicalDirection.Forward);
+            }
+
+
+            foreach (TextRange range in ranges)
+            {
+                range.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(Colors.Red));
+                range.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+            }
+
+
+        }
+
+        
     }
 }
