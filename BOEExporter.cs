9aef9713371062ac2a48d5d1801194fae5c36e7c
vadeemb <vbarkhatov@cheo.on.ca>
2020-01-21 11:12:05 -0500

    Updated assemblies

--- BOEExporter.cs
+++ BOEExporter.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CrystalDecisions.ReportAppServer.Utilities;
 using System.IO;
 using RptToXml;
 using System.Threading;
