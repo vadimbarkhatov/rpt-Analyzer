456ec60fffa9991c0d4285da64208bf3e0754ed3
vadeemb <vbarkhatov@cheo.on.ca>
2021-05-28 13:31:53 -0400

    c

--- ReportItem.cs
+++ ReportItem.cs
@@ -30,6 +30,9 @@ namespace CHEORptAnalyzer
             {
                 Language = FastColoredTextBoxNS.Language.Custom,
                 ResultFilter = x => x.Descendants("Tables").Descendants("Field"),
+                ResultFormat = s =>
+                    s.Select(x => x.Value)
+                     .Combine("\r\n" + "\r\n"),
             },
             [CRElement.Command] = new CRSection
             {
@@ -82,65 +85,6 @@ namespace CHEORptAnalyzer
                     ).Combine("\r\n"),
             },
         };
-
-        //Parameters,
-
-        public static readonly CRSection CRField = new CRSection
-        {
-            Language = FastColoredTextBoxNS.Language.Custom,
-            ResultFilter = x => x.Descendants("Tables").Descendants("Field"),
-        };
-        public static readonly CRSection CRCommand = new CRSection
-        {
-            Language = FastColoredTextBoxNS.Language.SQL,
-            ResultFilter = x => x.Descendants("Command"),
-        };
-        public static readonly CRSection CRRecordFormula = new CRSection
-        {
-            Language = FastColoredTextBoxNS.Language.Custom,
-            ResultFilter = x => x.Where(y => y.Name == "DataDefinition").Elements("RecordSelectionFormula"),
-        };
-        public static readonly CRSection CRFormulaFields = new CRSection
-        {
-            Language = FastColoredTextBoxNS.Language.Custom,
-            ResultFilter = x => x.Where(y => y.Name == "DataDefinition").Elements("FormulaFieldDefinitions").Elements("FormulaFieldDefinition"),
-            ResultFormat = s =>
-                s.Select(x =>
-                    x.Attribute("FormulaName").Value + " : " + x.Attribute("ValueType").Value.Replace("Field", "") +
-                    "\r\n" + "{" +
-                    "\r\n" + x.Value.AppendToNewLine("\t") +
-                    "\r\n" + "}")
-                 .Combine("\r\n" + "\r\n"),
-        };
-        public static readonly CRSection CRGroupFormula = new CRSection
-        {
-            Language = FastColoredTextBoxNS.Language.Custom,
-            ResultFilter = x => x.Where(y => y.Name == "DataDefinition").Elements("GroupSelectionFormula"),
-        };
-        public static readonly CRSection CRTableLinks = new CRSection
-        {
-            Language = FastColoredTextBoxNS.Language.Custom,
-            ResultFilter = x => x.Descendants("TableLinks").Elements("TableLink"),
-            ResultFormat = s =>
-                s.Select(x =>
-                    x.Attribute("JoinType").Value + " "
-                    + x.Elements("SourceFields").Elements("Field").First().Attribute("FormulaName").Value + " On "
-                    + x.Elements("DestinationFields").Elements("Field").First().Attribute("FormulaName").Value)
-                 .Combine("\r\n" + "\r\n"),
-        };
-        public static readonly CRSection CRParameters = new CRSection
-        {
-            Language = FastColoredTextBoxNS.Language.Custom,
-            ResultFilter = x => x.Where(y => y.Name == "DataDefinition").Elements("ParameterFieldDefinitions").Elements("ParameterFieldDefinition"),
-
-            ResultFormat = s =>
-                s.Select(x => //Parameters that are linked to a subreport have a different schema and need to be handled seperately
-                    x.Attribute("IsLinkedToSubreport") != null ?
-                    "{" + x.Attribute("Name").Value + "} -> \"" + x.Attribute("ReportName").Value + "\"" :
-                    (x.Attribute("ParameterFieldUsage").Value == "NotInUse" ? "//" : "") + x.Attribute("FormulaName").Value + " : " + x.Attribute("ValueType").Value.Replace("Field", "")
-                ).Combine("\r\n"),
-        };
-
 
         public string Text
         {
