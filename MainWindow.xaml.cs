b186441e31862baad5f27bca747e877092bc9c7b
Vadim Barkhatov <vadim.barkhatov@gmail.com>
2024-12-03 08:40:55 -0500

    removed testwindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,20 @@
-﻿namespace CHEORptAnalyzer
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using FastColoredTextBoxNS;
+using LiteDB;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace CHEORptAnalyzer
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.Diagnostics;
-    using System.IO;
-    using System.Linq;
-    using System.Text.RegularExpressions;
-    using System.Threading.Tasks;
-    using System.Windows;
-    using System.Windows.Forms;
-    using System.Xml.Linq;
-    using FastColoredTextBoxNS;
-    using LiteDB;
-    using Microsoft.WindowsAPICodePack.Dialogs;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
